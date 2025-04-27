6ddf89bf281e7db1f3855d1027998a72b972d799
mxblsdl <maxblasdel@gmail.com>
2025-04-27 09:06:58 -0700

    log errors in middleware

--- internal/middleware/middleware.go
+++ internal/middleware/middleware.go
@@ -19,6 +19,7 @@ func RecoveryMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		defer func() {
 			if err := recover(); err != nil {
+				logger.LogError("Recovered from panic: %v", err)
 				http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 			}
 		}()
