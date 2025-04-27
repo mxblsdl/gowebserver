cc2553cfd91bb328fd353375ab1536e333e9e154
mxblsdl <maxblasdel@gmail.com>
2025-04-27 09:06:46 -0700

    add colors for logging

---
+++ internal/logger/colors.go
@@ -0,0 +1,11 @@
+package logger
+
+const blue = "\033[34m"
+const red = "\033[31m"
+const green = "\033[32m"
+const yellow = "\033[33m"
+const magenta = "\033[35m"
+// const cyan = "\033[36m"
+// const white = "\033[37m"
+
+const reset = "\033[0m"
--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -7,26 +7,44 @@ import (
 
 var Logger *log.Logger
 
-func InitLogger() {
+var logLevel = "INFO"
+
+func InitLogger(level string) {
+	logLevel = level
 	Logger = log.New(os.Stdout, "", log.Ldate|log.Ltime|log.Lshortfile)
 }
 
+func shouldLog(level string) bool {
+	levels := map[string]int{"DEBUG": 1, "INFO": 2, "WARNING": 3, "ERROR": 4, "FATAL": 5}
+	return levels[level] >= levels[logLevel]
+}
+
 func LogInfo(message string, v ...interface{}) {
-	Logger.Printf("[INFO] "+message, v...)
+	if shouldLog("INFO") {
+		Logger.Printf(green+"[INFO] "+reset+message, v...)
+	}
 }
 
 func LogWarning(message string, v ...interface{}) {
-	Logger.Printf("[WARNING] "+message, v...)
+	if shouldLog("WARNING") {
+		Logger.Printf(magenta+"[WARNING] "+reset+message, v...)
+	}
+}
+
+func LogDebug(message string, v ...interface{}) {
+	if shouldLog("DEBUG") {
+		Logger.Printf(yellow+"[DEBUG] "+reset+message, v...)
+	}
 }
 
 func LogError(message string, v ...interface{}) {
-	Logger.Printf("[ERROR] "+message, v...)
+	if shouldLog("ERROR") {
+		Logger.Printf(red+"[ERROR] "+message+reset, v...)
+	}
 }
 
 func LogFatal(message string, v ...interface{}) {
-	Logger.Fatalf("[FATAL] "+message, v...)
-}
-
-func LogPanic(message string, v ...interface{}) {
-	Logger.Panicf("[PANIC] "+message, v...)
+	if shouldLog("FATAL") {
+		Logger.Fatalf(blue+"[FATAL] "+message+reset, v...)
+	}
 }
